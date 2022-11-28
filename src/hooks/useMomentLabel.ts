172a9d1e3f5435a5f98f9a8f052bd1b63b257fd0
Michell Ocaña <ocana.1997@hotmail.com>
2022-11-28 20:23:13 -0300

    fix: round time in useMomentLabel hook

--- src/hooks/useMomentLabel.ts
+++ src/hooks/useMomentLabel.ts
@@ -2,8 +2,9 @@ import { useMemo } from 'react'
 
 export function useMomentLabel(time: number) {
   return useMemo(() => {
-    const minutes = Math.floor(time / 60)
-    const seconds = time % 60
+    const roundedTime = Math.round(time)
+    const minutes = Math.floor(roundedTime / 60)
+    const seconds = roundedTime % 60
     const normalizeTime = (time: number) => time.toString().padStart(2, '0')
     return `${normalizeTime(minutes)}:${normalizeTime(seconds)}`
   }, [time])
