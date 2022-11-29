3218bd68c85e826f1b7d80c56db528dea0d96487
Michell Ocaña <ocana.1997@hotmail.com>
2022-11-28 21:02:35 -0300

    feat: add extended moment

--- src/contexts/MomentProvider.tsx
+++ src/contexts/MomentProvider.tsx
@@ -11,8 +11,13 @@ export default function MomentProvider({ children }: PropsWithChildren) {
   const [moments] = useState<Moment[]>([
     {
       name: 'Moment 1',
-      start: 95,
-      end: 110,
+      start: 98,
+      end: 108,
+    },
+    {
+      name: 'Moment 1 extended',
+      start: 88,
+      end: 118,
     },
   ])
   const [currentMoment, setCurrentMoment] = useState(moments[0])
