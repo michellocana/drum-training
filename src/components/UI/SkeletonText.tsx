1adedcea2b51397a5ca493ea99d7bb0c82085cbf
Michell Ocaña <ocana.1997@hotmail.com>
2023-04-09 02:24:15 -0300

    feat: implement SkeletonText component

---
+++ src/components/UI/SkeletonText.tsx
@@ -0,0 +1,35 @@
+import cn from 'classnames'
+import { useRef, useLayoutEffect, useState } from 'react'
+import s from './SkeletonText.module.css'
+
+type SkeletonTextProps = {
+  fontSize: number
+  lineHeight: number
+  text: string
+  className?: string
+}
+
+export default function SkeletonText({ text, className, fontSize, lineHeight }: SkeletonTextProps) {
+  const ref = useRef<HTMLSpanElement>(null)
+  const [rect, setRect] = useState<DOMRect>()
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      setRect(ref.current.getBoundingClientRect())
+    }
+  }, [text])
+
+  console.log(rect)
+
+  return (
+    <span
+      className={cn(s.wrapper, className)}
+      style={{ fontSize, lineHeight, width: rect?.width, height: rect?.height }}
+      aria-hidden
+    >
+      <span className={s.text} ref={ref}>
+        {text}
+      </span>
+    </span>
+  )
+}
